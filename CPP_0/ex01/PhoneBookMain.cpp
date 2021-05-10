330e6dd7c60505189027a431058705ea89add48b
pichkasik <sasha.marakulin@mail.ru>
2021-05-10 22:11:56 +0300

    Make a main loop

--- CPP_0/ex01/PhoneBookMain.cpp
+++ CPP_0/ex01/PhoneBookMain.cpp
@@ -13,12 +13,32 @@
 /* **************************************************************************** */
 
 
-#include "PhoneBookMain.hpp"
-#include "Contact.hpp"
-#include "Search.hpp"
+#include "General.hpp"
 
 int main(){
 	Contact contacts[8];
-
-	return (0);
+	int indexContact = 0;
+	int highestIndexContact = 7;
+	std::string commandString;
+	Add addCommand;
+	Search searchCommand;
+	while(1){
+		std::cout << "Write one of the following command (ADD, SEARCH, EXIT): ";
+		std::cin >> commandString;
+		if (commandString == "ADD"){
+			if (addCommand.executeCommandAdd(contacts, indexContact, highestIndexContact)){
+				indexContact++;
+			}
+		}
+		else if (commandString == "SEARCH"){
+			searchCommand.executeCommandSearch(contacts, indexContact);
+		}
+		else if (commandString == "EXIT"){
+			std::cout << "Bye! Bye!\n";
+			return (0);
+		}
+		else{
+			std::cout << "[-] Error: '" + commandString + "' " + " is invalid command. Try again:\n";
+		}
+	}
 }
