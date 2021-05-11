cc47be1797ce4f1b8c9af6074768cd80e91eeab8
pichkasik <sasha.marakulin@mail.ru>
2021-05-11 15:37:46 +0300

    first implementation

---
+++ CPP_1/ex01/ex01.cpp
@@ -0,0 +1,28 @@
+/* **************************************************************************** */
+/*                                                                      .       */
+/*   ex01.cpp.c                                                        / V\     */
+/*                                                                   / `  /     */
+/*   By: tilda      <tilda@student.21-school.ru.fr>                 <<   |      */
+/*                                                                  /    |      */
+/*   Created: 5/11/21 by tilda                                    /      |      */
+/*                                                              /        |      */
+/*                                                            /    \  \ /       */
+/*                                                           (      ) | |       */
+/*                                                   ________|   _/_  | |       */
+/*                                                 <__________\______)\__)      */
+/* **************************************************************************** */
+
+#include <iostream>
+
+void memoryLeak()
+{
+	std::string* panther = new std::string("String panther");
+	std::cout << *panther << std::endl;
+	delete panther;
+}
+
+int main(){
+	memoryLeak();
+	while (1);
+	return (0);
+}
