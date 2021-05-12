f4d8eeb5f69b96dff107bdd81728bbc07551329a
pichkasik <sasha.marakulin@mail.ru>
2021-05-12 13:49:29 +0300

    Set the reference to HumanA and pointer to HumanA

---
+++ CPP_1/ex06/main.cpp
@@ -0,0 +1,34 @@
+/* **************************************************************************** */
+/*                                                                      .       */
+/*   main.cpp.c                                                        / V\     */
+/*                                                                   / `  /     */
+/*   By: tilda      <tilda@student.21-school.ru.fr>                 <<   |      */
+/*                                                                  /    |      */
+/*   Created: 5/12/21 by tilda                                    /      |      */
+/*                                                              /        |      */
+/*                                                            /    \  \ /       */
+/*                                                           (      ) | |       */
+/*                                                   ________|   _/_  | |       */
+/*                                                 <__________\______)\__)      */
+/* **************************************************************************** */
+
+#include "HumanA.hpp"
+#include "HumanB.hpp"
+
+int main(){
+	{
+		Weapon club = Weapon("crude spiked club");
+		HumanA bob("Bob", club);
+		bob.attack();
+		club.setType("some other type of club");
+		bob.attack();
+	}
+	{
+		Weapon club = Weapon("crude spiked club");
+		HumanB jim("Jim");
+		jim.setWeapon(club);
+		jim.attack();
+		club.setType("some other type of club");
+		jim.attack();
+	}
+}
