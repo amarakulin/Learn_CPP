d4201064062bddf26b535a6424b475a18f6688b3
pichkasik <sasha.marakulin@mail.ru>
2021-05-13 22:19:16 +0300

    Create firts test

--- CPP_3/ex00/main.cpp
+++ CPP_3/ex00/main.cpp
@@ -12,4 +12,24 @@
 /*                                                 <__________\______)\__)      */
 /* **************************************************************************** */
 
+#include "FragTrap.hpp"
 
+int main(){
+	FragTrap Rick("Rick");
+	FragTrap Nick("Nick");
+
+	Rick.meleeAttack("attacks something");
+	Rick.rangeAttack("attacks a box");
+	Rick.takeDamage(50);
+	Rick.vaulthunter_dot_exe("TARGET");
+	Rick.vaulthunter_dot_exe("TARGET1");
+	Rick.vaulthunter_dot_exe("TARGET2");
+	Rick.vaulthunter_dot_exe("TARGET3");
+	Rick.vaulthunter_dot_exe("TARGET4");
+	Rick.vaulthunter_dot_exe("TARGET5");
+	Rick.beRepaired(20);
+	Rick.takeDamage(90);
+	Rick.takeDamage(90);
+
+	return (0);
+}
