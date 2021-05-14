ac3a6b67bab23ae805f1c4fb9bb49507277e67e9
pichkasik <sasha.marakulin@mail.ru>
2021-05-14 16:38:43 +0300

    Make a own method

--- CPP_3/ex03/NinjaTrap.cpp
+++ CPP_3/ex03/NinjaTrap.cpp
@@ -20,6 +20,19 @@ NinjaTrap::NinjaTrap(std::string name) : ClapTrap(name, 60, 120, 60, 5, 0, 40, 1
 
 }
 
+NinjaTrap::~NinjaTrap(){
+	std::cout << "NINJA " << name
+			  << " with level " << level
+			  << ". Left " << hitPoints
+			  << " hits and " << energyPoints
+			  << " energy points!"
+			  << " He has been distructed" << std::endl;
+}
+
+NinjaTrap::NinjaTrap(const NinjaTrap &copy) : ClapTrap(copy){
+	operator=(copy);
+}
+
 NinjaTrap &NinjaTrap::operator=(const NinjaTrap &assign){
 	if (this == &assign){
 		this->hitPoints = assign.hitPoints;
@@ -30,26 +43,20 @@ NinjaTrap &NinjaTrap::operator=(const NinjaTrap &assign){
 	return *this;
 }
 
-NinjaTrap::NinjaTrap(const NinjaTrap &copy){
-	operator=(copy);
-}
-
 void NinjaTrap::ninjaShoebox(const ClapTrap &obj){
-	std::cout << "get a ClapTrap obj" << std::endl;
+	std::cout << "get a ClapTrap obj  with name " << obj.name << std::endl;
 }
 
 void NinjaTrap::ninjaShoebox(const FragTrap &obj){
-	std::cout << "get a FragTrap obj" << std::endl;
-
+	std::cout << "get a FragTrap obj with name " << obj.name << std::endl;
 }
 
 void NinjaTrap::ninjaShoebox(const ScavTrap &obj){
-	std::cout << "get a ScavTrap obj" << std::endl;
-
+	std::cout << "get a ScavTrap obj with name " << obj.name << std::endl;
 }
 
 void NinjaTrap::ninjaShoebox(const NinjaTrap &obj){
-	std::cout << "get a NinjaTrap obj" << std::endl;
-
+	std::cout << "get a NinjaTrap obj with name " << obj.name << std::endl;
 }
+
 
