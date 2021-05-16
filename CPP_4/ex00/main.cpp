05cf17a1a6e463ad7884ed4d8775bb64e0c6d404
pichkasik <sasha.marakulin@mail.ru>
2021-05-16 14:23:27 +0300

    Add tests

--- CPP_4/ex00/main.cpp
+++ CPP_4/ex00/main.cpp
@@ -15,14 +15,19 @@
 #include "Victim.hpp"
 #include "Sorcerer.hpp"
 #include "Peon.hpp"
+#include "Otter.hpp"
 
 int main()
 {
 	Sorcerer robert("Robert", "the Magnificent");
+	Sorcerer noName;
 	Victim jim("Jimmy");
 	Peon joe("Joe");
-	std::cout << robert << jim << joe;
+	Otter otto("Otto");
+	std::cout << robert << jim << joe << otto;
 	robert.polymorph(jim);
 	robert.polymorph(joe);
+	robert.polymorph(otto);
+	noName.polymorph(otto);
 	return 0;
 }
