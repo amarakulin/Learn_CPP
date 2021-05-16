5f64acbd0d62ba93ebae23082267c55defec7112
pichkasik <sasha.marakulin@mail.ru>
2021-05-16 22:55:35 +0300

    init commit

---
+++ CPP_4/ex02/AssaultTerminator.hpp
@@ -0,0 +1,26 @@
+/* **************************************************************************** */
+/*                                                                      .       */
+/*   AssaultTerminator.hpp.h                                                        / V\     */
+/*                                                                   / `  /     */
+/*   By: tilda      <tilda@student.21-school.ru.fr>                 <<   |      */
+/*                                                                  /    |      */
+/*   Created: 5/16/21 by tilda                                    /      |      */
+/*                                                              /        |      */
+/*                                                            /    \  \ /       */
+/*                                                           (      ) | |       */
+/*                                                   ________|   _/_  | |       */
+/*                                                 <__________\______)\__)      */
+/* **************************************************************************** */
+
+
+#ifndef CPP_4_ASSAULTTERMINATOR_HPP
+#define CPP_4_ASSAULTTERMINATOR_HPP
+
+
+class AssaultTerminator
+{
+
+};
+
+
+#endif //CPP_4_ASSAULTTERMINATOR_HPP
