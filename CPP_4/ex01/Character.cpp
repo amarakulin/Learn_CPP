db3a3df9d6bd179bc49e0578503a8f53fe7b19d7
pichkasik <sasha.marakulin@mail.ru>
2021-05-16 17:47:12 +0300

    almost set fields methods by subj

--- CPP_4/ex01/Character.cpp
+++ CPP_4/ex01/Character.cpp
@@ -14,3 +14,91 @@
 
 
 #include "Character.hpp"
+
+
+Character::Character(){
+
+}
+
+Character::Character(const std::string &name){
+	this->_name = name;
+	this->_actionPoints = 40;
+	this->_weapon = nullptr;
+}
+
+Character::Character(const Character &copy){
+	operator=(copy);
+}
+
+Character &Character::operator=(const Character &assign){
+	if (this != &assign){
+		this->_name = assign.getName();
+		this->_actionPoints = assign.getActionPoints();
+		this->_weapon = assign.getWeapon();
+	}
+	return *this;
+}
+
+Character::~Character(){
+
+}
+
+const std::string &Character::getName() const{
+	return _name;
+}
+
+void Character::setName(const std::string &name){
+	Character::_name = name;
+}
+
+int Character::getActionPoints() const{
+	return _actionPoints;
+}
+
+void Character::setActionPoints(int actionPoints){
+	Character::_actionPoints = actionPoints;
+}
+
+AWeapon *Character::getWeapon() const{
+	return _weapon;
+}
+
+void Character::setWeapon(AWeapon *weapon){
+	Character::_weapon = weapon;
+}
+
+void Character::recoverAP(){
+	this->_actionPoints += _apToRecover;
+	if (this->_actionPoints > 40){
+		this->_actionPoints = 40;
+	}
+}
+
+void Character::attack(Enemy *enemy){
+	if (this->_weapon != nullptr){
+		std::cout << BOLDGREEN << this->_name << " has " <<
+				  this->_actionPoints << " AP and is unarmed" <<
+				  RESET << std::endl;
+		return;
+	}
+	if (this->_weapon->getApcost() <= this->_actionPoints){
+		enemy->takeDamage(this->_weapon->getDamage());
+		this->_actionPoints -= this->_weapon->getApcost();
+		std::cout << BOLDGREEN << this->_name <<
+				  " attacks " << enemy->getType() <<
+				  " with a " << this->_weapon->getName() <<
+				  RESET << std::endl;
+		if (enemy->getHp() <= 0){
+			delete enemy;
+		}
+	}
+	else{
+		std::cout << BOLDGREEN << this->_name <<
+				  " CAN'T attacks " << enemy->getType() <<
+				  RESET << std::endl;
+	}
+}
+
+void Character::equip(AWeapon *weapon){
+	setWeapon(weapon);
+}
--- CPP_4/ex01/Character.hpp
+++ CPP_4/ex01/Character.hpp
@@ -16,11 +16,34 @@
 #ifndef CPP_4_CHARACTER_HPP
 #define CPP_4_CHARACTER_HPP
 
+#include "AWeapon.hpp"
+#include "Enemy.hpp"
 
-class Character
-{
+class Character{
+public:
+	Character();
+	Character(std::string const & name);
+	Character(Character const &copy);
+	Character& operator=(const Character &assign);
+	~Character();
 
+	const std::string &getName() const;
+	int getActionPoints() const;
+	AWeapon *getWeapon() const;
+	void setName(const std::string &_name);
+	void setActionPoints(int _actionPoints);
+	void setWeapon(AWeapon *_weapon);
+
+	void recoverAP();
+	void equip(AWeapon* weapon);
+	void attack(Enemy* enemy);
+
+private:
+	std::string _name;
+	int _actionPoints;
+	AWeapon *_weapon;
+	const int _apToRecover = 10;
 };
 
 
-#endif //CPP_4_CHARACTER_HPP
+#endif
