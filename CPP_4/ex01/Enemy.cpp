24074924bbf0d9649539264b9d34866095b0f65d
pichkasik <sasha.marakulin@mail.ru>
2021-05-16 16:51:43 +0300

    set fields methods by subj

--- CPP_4/ex01/Enemy.cpp
+++ CPP_4/ex01/Enemy.cpp
@@ -14,3 +14,59 @@
 
 
 #include "Enemy.hpp"
+#include "Colors.hpp"
+
+Enemy::Enemy(){
+	setParamConstructor(0, "No type");
+}
+
+Enemy::Enemy(int hp, const std::string &type){
+	setParamConstructor(hp, type);
+}
+
+Enemy::Enemy(const Enemy &copy){
+	operator=(copy);
+}
+
+Enemy &Enemy::operator=(const Enemy &assign){
+	if (this != &assign){
+		this->setHp(assign.getHp());
+		this->setType(assign.getType());
+	}
+	return *this;
+}
+
+Enemy::~Enemy(){
+
+}
+
+void Enemy::setParamConstructor(int hp, std::string const & type){
+	std::cout << BOLDGREEN "Enemy constructor" RESET << std::endl;
+	setHp(hp);
+	setType(type);
+}
+
+int Enemy::getHp() const{
+	return _hp;
+}
+
+void Enemy::setHp(int hp){
+	_hp = hp;
+}
+
+const std::string &Enemy::getType() const{
+	return _type;
+}
+
+void Enemy::setType(const std::string &type){
+	_type = type;
+}
+
+void Enemy::takeDamage(int damage){
+	if (damage > 0 ){
+		this->_hp -= damage;
+		if (this->_hp < 0){
+			this->_hp = 0;
+		}
+	}
+}
--- CPP_4/ex01/Enemy.hpp
+++ CPP_4/ex01/Enemy.hpp
@@ -16,11 +16,30 @@
 #ifndef CPP_4_ENEMY_HPP
 #define CPP_4_ENEMY_HPP
 
+#include "iostream"
 
-class Enemy
-{
+class Enemy{
+public:
+	Enemy();
+	Enemy(int hp, std::string const & type);
+	Enemy(Enemy const &copy);
+	Enemy& operator=(const Enemy &assign);
+	virtual ~Enemy();
 
+	int getHp() const;
+	const std::string &getType() const;
+	void setHp(int hp);
+	void setType(const std::string &type);
+
+	virtual void takeDamage(int damage);
+
+protected:
+	void setParamConstructor(int hp, std::string const & type);
+
+private:
+	int _hp;
+	std::string _type;
 };
 
 
-#endif //CPP_4_ENEMY_HPP
+#endif
