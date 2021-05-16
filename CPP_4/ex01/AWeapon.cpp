5f4c0830398a54fb9533d6a785951db4af8d1e85
pichkasik <sasha.marakulin@mail.ru>
2021-05-16 16:01:41 +0300

    set fields methods by subj

--- CPP_4/ex01/AWeapon.cpp
+++ CPP_4/ex01/AWeapon.cpp
@@ -14,3 +14,54 @@
 
 
 #include "AWeapon.hpp"
+
+AWeapon::AWeapon(){
+	setName("No name");
+	setApcost(0);
+	setDamage(0);
+}
+
+AWeapon::AWeapon(const std::string &name, int apcost, int damage){
+	setName(name);
+	setApcost(apcost);
+	setDamage(damage);
+}
+
+AWeapon::AWeapon(const AWeapon &copy){
+	operator=(copy);
+}
+
+AWeapon &AWeapon::operator=(const AWeapon &assign){
+	if (this != &assign){
+		this->name = assign.getName();
+		this->apcost = assign.getApcost();
+		this->damage = assign.getDamage();
+	}
+	return *this;
+}
+
+AWeapon::~AWeapon(){}
+
+const std::string &AWeapon::getName() const{
+	return name;
+}
+
+void AWeapon::setName(const std::string &name){
+	AWeapon::name = name;
+}
+
+int AWeapon::getApcost() const{
+	return apcost;
+}
+
+void AWeapon::setApcost(int apcost){
+	AWeapon::apcost = apcost;
+}
+
+int AWeapon::getDamage() const{
+	return damage;
+}
+
+void AWeapon::setDamage(int damage){
+	AWeapon::damage = damage;
+}
--- CPP_4/ex01/AWeapon.hpp
+++ CPP_4/ex01/AWeapon.hpp
@@ -16,11 +16,29 @@
 #ifndef CPP_4_AWEAPON_HPP
 #define CPP_4_AWEAPON_HPP
 
+#include "iostream"
 
-class AWeapon
-{
+class AWeapon{
+public:
+	AWeapon();
+	AWeapon(std::string const &name, int apcost, int damage);
+	AWeapon(AWeapon const &copy);
+	AWeapon& operator=(const AWeapon &assign);
+	virtual ~AWeapon();
+	
+	virtual void attack() const = 0;
+	void setName(const std::string &name);
+	void setApcost(int apcost);
+	void setDamage(int damage);
+	const std::string &getName() const;
+	int getApcost() const;
+	int getDamage() const;
 
+private:
+	std::string name;
+	int apcost;
+	int damage;
 };
 
 
-#endif //CPP_4_AWEAPON_HPP
+#endif
