76c988b81c91643774b523ef3b04efe6cd88042b
pichkasik <sasha.marakulin@mail.ru>
2021-05-17 13:20:09 +0300

    Create by subj

--- CPP_4/ex02/TacticalMarine.cpp
+++ CPP_4/ex02/TacticalMarine.cpp
@@ -14,3 +14,31 @@
 
 
 #include "TacticalMarine.hpp"
+
+TacticalMarine::TacticalMarine() : ISpaceMarine(){
+	std::cout << "Tactical Marine ready for battle!" << std::endl;
+}
+
+TacticalMarine::TacticalMarine(const TacticalMarine &copy){
+	operator=(copy);
+}
+
+TacticalMarine &TacticalMarine::operator=(const TacticalMarine &assign){
+	return *this;
+}
+
+TacticalMarine::~TacticalMarine(){
+	std::cout << "Aaargh..." << std::endl;
+}
+
+void TacticalMarine::battleCry() const{
+	std::cout << "For the holy PLOT!" << std::endl;
+}
+
+void TacticalMarine::rangedAttack() const{
+	std::cout << "* attacks with a bolter *" << std::endl;
+}
+
+void TacticalMarine::meleeAttack() const{
+	std::cout << "* attacks with a chainsword *" << std::endl;
+}
--- CPP_4/ex02/TacticalMarine.hpp
+++ CPP_4/ex02/TacticalMarine.hpp
@@ -16,11 +16,19 @@
 #ifndef CPP_4_TACTICALMARINE_HPP
 #define CPP_4_TACTICALMARINE_HPP
 
+#include "ISpaceMarine.hpp"
+#include <iostream>
 
-class TacticalMarine
-{
+class TacticalMarine : virtual public ISpaceMarine{
+public:
+	TacticalMarine();
+	TacticalMarine(TacticalMarine const &copy);
+	TacticalMarine& operator=(const TacticalMarine &assign);
+	~TacticalMarine();
 
+	void battleCry() const;
+	void rangedAttack() const;
+	void meleeAttack() const;
 };
 
-
-#endif //CPP_4_TACTICALMARINE_HPP
+#endif
