e82b3b29f597f8b24a100ce6da616e0a3e77fdfc
pichkasik <sasha.marakulin@mail.ru>
2021-05-17 13:03:45 +0300

    set by subj

--- CPP_4/ex02/ISpaceMarine.hpp
+++ CPP_4/ex02/ISpaceMarine.hpp
@@ -16,4 +16,12 @@
 #ifndef CPP_4_ISPACEMARINE_HPP
 #define CPP_4_ISPACEMARINE_HPP
 
-#endif //CPP_4_ISPACEMARINE_HPP
+class ISpaceMarine{
+public:
+	virtual ~ISpaceMarine(){}
+	virtual ISpaceMarine* clone() const = 0;
+	virtual void battleCry() const = 0;
+	virtual void rangedAttack() const = 0;
+	virtual void meleeAttack() const = 0;
+};
+#endif
--- CPP_4/ex02/ISquad.hpp
+++ CPP_4/ex02/ISquad.hpp
@@ -16,4 +16,14 @@
 #ifndef CPP_4_ISQUAD_HPP
 #define CPP_4_ISQUAD_HPP
 
-#endif //CPP_4_ISQUAD_HPP
+#include "ISpaceMarine.hpp"
+
+class ISquad{
+public:
+	virtual ~ISquad(){}
+	virtual int getCount const = 0;
+	virtual ISpaceMarine* getUnit(int idxUnit) const = 0;
+	virtual int push(ISpaceMarine*) = 0;
+};
+
+#endif
