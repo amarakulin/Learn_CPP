744b241af7b53fa808382312fc81b2ccbb74742f
pichkasik <sasha.marakulin@mail.ru>
2021-05-17 21:50:35 +0300

    Create by subj

--- CPP_4/ex03/MateriaSource.cpp
+++ CPP_4/ex03/MateriaSource.cpp
@@ -14,3 +14,56 @@
 
 
 #include "MateriaSource.hpp"
+
+MateriaSource::MateriaSource(){
+	for (int i = 0; i < 4; i++){
+		this->_materials[i] = nullptr;
+	}
+}
+
+MateriaSource::MateriaSource(const MateriaSource &copy){
+	operator=(copy);
+}
+
+MateriaSource &MateriaSource::operator=(const MateriaSource &assign){
+	if (this != &assign){
+		deleteMaterials();
+		for(int i = 0; i < 4; i++){
+			this->_materials[i] = assign._materials[i];
+		}
+	}
+	return *this;
+}
+
+MateriaSource::~MateriaSource(){
+
+}
+
+void MateriaSource::learnMateria(AMateria *m){
+	for (int i = 0; i < 4; i++){
+		if (this->_materials[i] == nullptr){
+			this->_materials[i] = m->clone();
+			std::cout << "learnMateria " << this->_materials[i]->getType() << std::endl;
+			return;
+		}
+	}
+}
+
+AMateria *MateriaSource::createMateria(const std::string &type){
+	for (int i = 0; i < 4; i++){
+		if (this->_materials[i]->getType() == type){
+			std::cout << "createMateria " << this->_materials[i]->getType() << std::endl;
+			return this->_materials[i]->clone();
+		}
+	}
+	return 0;
+}
+
+
+void MateriaSource::deleteMaterials(){
+	int i = 0;
+	while(i < 4 && this->_materials[i] != nullptr){
+		delete this->_materials[i];
+		i++;
+	}
+}
--- CPP_4/ex03/MateriaSource.hpp
+++ CPP_4/ex03/MateriaSource.hpp
@@ -16,11 +16,24 @@
 #ifndef CPP_4_MATERIASOURCE_HPP
 #define CPP_4_MATERIASOURCE_HPP
 
+#include "IMateriaSource.hpp"
+#include "AMateria.hpp"
 
-class MateriaSource
-{
+class MateriaSource : virtual public IMateriaSource{
+public:
+	MateriaSource();
+	MateriaSource(MateriaSource const &copy);
+	MateriaSource& operator=(const MateriaSource &assign);
+	~MateriaSource();
+
+	void learnMateria(AMateria *m);
+	AMateria* createMateria(std::string const &type);
+
+private:
+	AMateria *_materials[4];
+	void deleteMaterials();
 
 };
 
 
-#endif //CPP_4_MATERIASOURCE_HPP
+#endif
