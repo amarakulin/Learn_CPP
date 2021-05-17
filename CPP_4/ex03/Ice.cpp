568ceb0ba4bb020a1b9d239854e8755ed490bdbf
pichkasik <sasha.marakulin@mail.ru>
2021-05-17 21:10:54 +0300

    Create by subj

--- CPP_4/ex03/Ice.cpp
+++ CPP_4/ex03/Ice.cpp
@@ -14,3 +14,40 @@
 
 
 #include "Ice.hpp"
+
+Ice::Ice() : AMateria(){
+	this->setType("ice");
+}
+
+Ice::Ice(const Ice &copy){
+	operator=(copy);
+}
+
+Ice &Ice::operator=(const Ice &assign){
+	if (this != &assign){
+		this->setXp(assign.getXP());
+		this->setType(assign.getType());
+	}
+	return *this;
+}
+
+Ice::~Ice(){
+
+}
+
+std::string const &Ice::getType() const{
+	return AMateria::getType();
+}
+
+unsigned int Ice::getXP() const{
+	return AMateria::getXP();
+}
+
+AMateria *Ice::clone() const{
+	return (new Ice(*this));
+}
+
+void Ice::use(ICharacter &target){
+	AMateria::use(target);
+	std::cout << "* shoots an ice bolt at  " << target.getName() << " *" << std::endl;
+}
--- CPP_4/ex03/Ice.hpp
+++ CPP_4/ex03/Ice.hpp
@@ -16,11 +16,20 @@
 #ifndef CPP_4_ICE_HPP
 #define CPP_4_ICE_HPP
 
+#include "AMateria.hpp"
 
-class Ice
-{
+class Ice :  virtual public AMateria{
+public:
+	Ice();
+	Ice(Ice const &copy);
+	Ice& operator=(const Ice &assign);
+	~Ice();
 
+	std::string const & getType() const;
+	unsigned int getXP() const;
+	AMateria* clone() const;
+	void use(ICharacter& target);
 };
 
 
-#endif //CPP_4_ICE_HPP
+#endif
