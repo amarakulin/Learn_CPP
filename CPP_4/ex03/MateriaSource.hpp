9ae6b97e9387294645ad1234cfaec39ba8b0fd8b
pichkasik <sasha.marakulin@mail.ru>
2021-05-17 19:03:45 +0300

    Init commit

---
+++ CPP_4/ex03/MateriaSource.hpp
@@ -0,0 +1,26 @@
+/* **************************************************************************** */
+/*                                                                      .       */
+/*   MateriaSource.hpp                                                        / V\     */
+/*                                                                   / `  /     */
+/*   By: tilda      <tilda@student.21-school.ru.fr>                 <<   |      */
+/*                                                                  /    |      */
+/*   Created: 5/17/21 by tilda                                    /      |      */
+/*                                                              /        |      */
+/*                                                            /    \  \ /       */
+/*                                                           (      ) | |       */
+/*                                                   ________|   _/_  | |       */
+/*                                                 <__________\______)\__)      */
+/* **************************************************************************** */
+
+
+#ifndef CPP_4_MATERIASOURCE_HPP
+#define CPP_4_MATERIASOURCE_HPP
+
+
+class MateriaSource
+{
+
+};
+
+
+#endif //CPP_4_MATERIASOURCE_HPP
