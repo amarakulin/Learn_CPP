d6d0afaaa5fd0c9fcc3deb974be92b059920c9cb
pichkasik <sasha.marakulin@mail.ru>
2021-05-18 09:58:30 +0300

    Init commit

---
+++ CPP_5/ex01/Bureaucrat.cpp
@@ -0,0 +1,98 @@
+/* **************************************************************************** */
+/*                                                                      .       */
+/*   Bureaucrat.cpp                                                        / V\     */
+/*                                                                   / `  /     */
+/*   By: tilda      <tilda@student.21-school.ru.fr>                 <<   |      */
+/*                                                                  /    |      */
+/*   Created: 5/18/21 by tilda                                    /      |      */
+/*                                                              /        |      */
+/*                                                            /    \  \ /       */
+/*                                                           (      ) | |       */
+/*                                                   ________|   _/_  | |       */
+/*                                                 <__________\______)\__)      */
+/* **************************************************************************** */
+
+
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(){
+	this->_name = "No _name";
+	this->_grade = _minGrade;
+}
+
+Bureaucrat::Bureaucrat(const std::string &name, int grade){
+	this->_name = name;
+	try {
+		if(grade < this->_maxGrade){
+			throw Bureaucrat::GradeTooHighException();
+		}
+		else if (grade > this->_minGrade){
+			throw Bureaucrat::GradeTooLowException();
+		}
+		this->_grade = grade;
+	}
+	catch (std::exception &e){
+		std::cout << e.what() << std::endl;
+	}
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &copy){
+	operator=(copy);
+}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &assign){
+	if (this != &assign){
+		this->_name = assign.getName();
+		this->_grade = assign.getGrade();
+	}
+	return *this;
+}
+
+Bureaucrat::~Bureaucrat(){
+
+}
+
+int Bureaucrat::getGrade() const{
+	return _grade;
+}
+
+const std::string &Bureaucrat::getName() const{
+	return _name;
+}
+
+void Bureaucrat::incrementGrage(){
+	try{
+		if (this->_grade - 1 < _maxGrade){
+			throw Bureaucrat::GradeTooHighException();
+		}
+		this->_grade -= 1;
+	}
+	catch (std::exception &e){
+		std::cout << e.what() << std::endl;
+	}
+}
+
+void Bureaucrat::decrementGrage(){
+	try{
+		if (this->_grade + 1 > _minGrade){
+			throw Bureaucrat::GradeTooLowException();
+		}
+		this->_grade += 1;
+	}
+	catch (std::exception &e){
+		std::cout << e.what() << std::endl;
+	}
+}
+
+const char *Bureaucrat::GradeTooHighException::what() const throw(){
+	return "Grade too high";
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw(){
+	return "Grade too low";
+}
+
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &bureaucrat){
+	out << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade();
+	return out;
+}
---
+++ CPP_5/ex01/Bureaucrat.hpp
@@ -0,0 +1,50 @@
+/* **************************************************************************** */
+/*                                                                      .       */
+/*   Bureaucrat.hpp                                                        / V\     */
+/*                                                                   / `  /     */
+/*   By: tilda      <tilda@student.21-school.ru.fr>                 <<   |      */
+/*                                                                  /    |      */
+/*   Created: 5/18/21 by tilda                                    /      |      */
+/*                                                              /        |      */
+/*                                                            /    \  \ /       */
+/*                                                           (      ) | |       */
+/*                                                   ________|   _/_  | |       */
+/*                                                 <__________\______)\__)      */
+/* **************************************************************************** */
+
+
+#ifndef CPP_5_BUREAUCRAT_HPP
+#define CPP_5_BUREAUCRAT_HPP
+
+#include <iostream>
+#include <exception>
+
+class Bureaucrat{
+public:
+	Bureaucrat();
+	Bureaucrat(const std::string &name, int grade);
+	Bureaucrat(Bureaucrat const &copy);
+	Bureaucrat& operator=(const Bureaucrat &assign);
+	~Bureaucrat();
+
+	int getGrade() const;
+	const std::string &getName() const;
+	void incrementGrage();
+	void decrementGrage();
+	class GradeTooHighException : public std::exception{
+		const char *what() const throw();
+	};
+	class GradeTooLowException : public std::exception{
+		const char *what() const throw();
+	};
+
+private:
+	std::string _name;
+	int _grade;
+	static const int _minGrade = 150;
+	static const int _maxGrade = 1;
+};
+
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &bureaucrat);
+
+#endif
