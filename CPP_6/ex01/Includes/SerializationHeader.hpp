03f2368322d95a4cfbf31ea90e977038f6dfe53b
pichkasik <sasha.marakulin@mail.ru>
2021-05-20 20:40:47 +0300

    Init commit

---
+++ CPP_6/ex01/Includes/SerializationHeader.hpp
@@ -0,0 +1,19 @@
+/* **************************************************************************** */
+/*                                                                      .       */
+/*   SerializationHeader.hpp                                                        / V\     */
+/*                                                                   / `  /     */
+/*   By: tilda      <tilda@student.21-school.ru.fr>                 <<   |      */
+/*                                                                  /    |      */
+/*   Created: 5/20/21 by tilda                                    /      |      */
+/*                                                              /        |      */
+/*                                                            /    \  \ /       */
+/*                                                           (      ) | |       */
+/*                                                   ________|   _/_  | |       */
+/*                                                 <__________\______)\__)      */
+/* **************************************************************************** */
+
+
+#ifndef CPP_6_SERIALIZATIONHEADER_HPP
+#define CPP_6_SERIALIZATIONHEADER_HPP
+
+#endif //CPP_6_SERIALIZATIONHEADER_HPP
