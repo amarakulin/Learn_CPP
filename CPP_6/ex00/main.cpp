51bd77d0c3daca49fa5564521122a999472a25d0
pichkasik <sasha.marakulin@mail.ru>
2021-05-20 16:08:08 +0300

    Add convert functions

--- CPP_6/ex00/main.cpp
+++ CPP_6/ex00/main.cpp
@@ -15,16 +15,43 @@
 
 #include "Include/scalarConvertion.hpp"
 
+std::string getCharStr(double dl){
+	char ch;
+	std::string output;
+	int integer = static_cast <int> (dl);
+	if(std::isprint(integer)){
+		ch = static_cast <char> (integer);
+		output = "'" + std::to_string(ch) + "'";
+	}
+	else{
+		output = "Non displayable";
+	}
+	return output;
+}
+
+std::string getIntStr(double dl){
+	int integer = static_cast <int> (dl);
+	return std::to_string(integer);
+}
+
+std::string getFloatStr(double dl){
+	float fl = static_cast <float> (dl);
+	return std::to_string(fl);
+}
+
+std::string getDoubleStr(double dl){
+	return std::to_string(dl);
+}
+
 void scalarConvertion(char* arg){
 	double dl;
 	std::string str (arg);
 	std::string::size_type sz;
 	dl = std::stod(arg, &sz);
-	float fl = static_cast <float> (dl);
-	int integer = static_cast <int> (dl);
-	std::cout << "int: " << integer << std::endl;
-	std::cout << "float: " << fl << std::endl;
-	std::cout << "double: " << dl << std::endl;
+	std::cout << "char: " << getCharStr(dl) << std::endl;
+	std::cout << "int: " << getIntStr(dl) << std::endl;
+	std::cout << "float: " << getFloatStr(dl) << "f" << std::endl;
+	std::cout << "double: " << getDoubleStr(dl) << std::endl;
 
 }
 
