f970a9fce493acf4c5703e6855503ae3e11fa7dd
pichkasik <sasha.marakulin@mail.ru>
2021-05-21 22:20:40 +0300

    add tests and create indetify by reference

--- CPP_6/ex02/main.cpp
+++ CPP_6/ex02/main.cpp
@@ -13,12 +13,93 @@
 /* **************************************************************************** */
 
 #include "Include/Base.hpp"
+#include "Include/A.hpp"
+#include "Include/B.hpp"
+#include "Include/C.hpp"
 
-Base * generate(void);
-void identify_from_pointer(Base * p);
-void identify_from_reference( Base & p);
+
+
+static Base *getA(){
+	return (new A());
+}
+
+static Base *getB(){
+	return (new B());
+}
+
+static Base *getC(){
+	return (new C());
+}
+
+const t_createObj arrCreateObj[NUMBER_FUNCTIONS] = {
+		{.getObj = getA},
+		{.getObj = getB},
+		{.getObj = getC}
+};
+
+
+
+static void identify_from_pointer(Base * p){
+	A *objA = dynamic_cast <A*> (p);
+	B *objB = dynamic_cast <B*> (p);
+	C *objC = dynamic_cast <C*> (p);
+
+	if (objA){
+		std::cout << objA->getName() << std::endl;
+	}
+	else if (objB){
+		std::cout << objB->getName() << std::endl;
+	}
+	else if (objC){
+		std::cout << objC->getName() << std::endl;
+	}
+}
+
+void identify_from_reference( Base & p){
+	try{
+		A &objA = dynamic_cast <A&> (p);
+		std::cout << objA.getName() << std::endl;
+		return;
+	}
+	catch (std::exception &e){}
+
+	try{
+		B &objB = dynamic_cast <B&> (p);
+		std::cout << objB.getName() << std::endl;
+		return;
+	}
+	catch (std::exception &e){}
+
+	try{
+		C &objC = dynamic_cast <C&> (p);
+		std::cout << objC.getName() << std::endl;
+		return;
+	}
+	catch (std::exception &e){}
+}
+
+
+Base* generate(void){
+	int randIdx = rand() % NUMBER_FUNCTIONS;
+	return (arrCreateObj[randIdx].getObj());
+}
 
 int main(){
+	std::srand(std::time(NULL));
+	Base *basePointer1 = generate();
+	Base *basePointer2 = generate();
+	Base *basePointer3 = generate();
+	identify_from_pointer(basePointer1);
+	identify_from_pointer(basePointer2);
+	identify_from_pointer(basePointer3);
 
+	Base &baseRef1 = *generate();
+	Base &baseRef2 = *generate();
+	identify_from_reference(baseRef1);
+	identify_from_reference(baseRef2);
+
+	delete basePointer1;
+	delete basePointer2;
+	delete basePointer3;
 	return 0;
 }
