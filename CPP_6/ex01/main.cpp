bf01f897016a683bcb4129f1f29e6c8312b520e1
pichkasik <sasha.marakulin@mail.ru>
2021-05-21 19:30:19 +0300

    Refactor

--- CPP_6/ex01/main.cpp
+++ CPP_6/ex01/main.cpp
@@ -16,14 +16,9 @@
 
 void serializeStr(char **dataSerialize, int sizeStr){
 	int randSymbol;
-//	char *copy;
-//	copy = *dataSerialize;
-	char *ch;
 	for (int i = 0; i <  + sizeStr; i++){
 		randSymbol = 65 + rand() % 15;
 		std::cout << static_cast <char> (randSymbol);
-//		ch = reinterpret_cast<char*>(*dataSerialize);
-//		*ch = static_cast <char> (randSymbol);
 		**dataSerialize = static_cast <char> (randSymbol);
 		(*dataSerialize)++;
 	}
@@ -31,15 +26,10 @@ void serializeStr(char **dataSerialize, int sizeStr){
 }
 
 void serializeInt(char **dataSerialize, int nbr, int sizeInt){
-//	ip = reinterpret_cast<int*>( &(static_cast<char*>(memory)[8]) );
 	int *ip;
 	ip = reinterpret_cast<int*>(*dataSerialize);
 	*ip = nbr;
-//	*dataSerialize = reinterpret_cast <char *> (nbr);
 	(*dataSerialize) += sizeInt;
-//	for(int i = 0; i < sizeInt; i++){
-//		(*dataSerialize)++;
-//	}
 }
 
 void* serialize(void){
@@ -66,7 +56,6 @@ void* serialize(void){
 }
 
 int deserializeInt(char** raw, int intSize){
-//	void* copy = static_cast <void *> (*raw);
 	int* integer = reinterpret_cast<int*>(*raw);
 	*(raw) += intSize;
 	std::cout << "DESIRILAZE INT: " << *integer << std::endl;
@@ -75,7 +64,6 @@ int deserializeInt(char** raw, int intSize){
 
 std::string deserializeStr(char** raw, int sizeInt){
 	int sizeStr;
-	char* literalStr;
 	std::string objStr;
 
 	sizeStr = deserializeInt(raw, sizeInt);
@@ -83,11 +71,6 @@ std::string deserializeStr(char** raw, int sizeInt){
 		objStr += **raw;
 		(*raw)++;
 	}
-	literalStr = new char[sizeStr];
-//	std::memcpy(literalStr, *raw, sizeStr);
-//	objStr = strdup(literalStr);
-//	*(raw) += sizeStr;
-	delete literalStr;
 	return objStr;
 }
 
@@ -106,24 +89,24 @@ Data* deserialize(void* raw){
 
 int main(){
 	std::srand(std::time(NULL));
-//	void* dataSerialize1 = serialize();
-//	void* dataSerialize2 = serialize();
+	void* dataSerialize1 = serialize();
+	void* dataSerialize2 = serialize();
 	void* dataSerialize3 = serialize();
 
-//	Data* data1 = deserialize(dataSerialize1);
-//	Data* data2 = deserialize(dataSerialize2);
+	Data* data1 = deserialize(dataSerialize1);
+	Data* data2 = deserialize(dataSerialize2);
 	Data* data3 = deserialize(dataSerialize3);
 
-//	std::cout << data1->str1 << "|" << data1->str2 << "|" << data1->integer << std::endl;
-//	std::cout << data2->str1 << "|" << data2->str2 << "|" << data2->integer << std::endl;
+	std::cout << data1->str1 << "|" << data1->str2 << "|" << data1->integer << std::endl;
+	std::cout << data2->str1 << "|" << data2->str2 << "|" << data2->integer << std::endl;
 	std::cout << data3->str1 << "|" << data3->str2 << "|" << data3->integer << std::endl;
 	std::cout << sizeof(data3->str1) << std::endl;
 	std::cout << sizeof(data3->integer) << std::endl;
 	std::cout << sizeof(data3->str2) << std::endl;
 
 
-//	delete data1;
-//	delete data2;
+	delete data1;
+	delete data2;
 	delete data3;
 	return (0);
 }
