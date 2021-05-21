0af062cc0619b200ecd45b2e22da0e54e91b916f
pichkasik <sasha.marakulin@mail.ru>
2021-05-21 13:56:03 +0300

    Add test

--- CPP_6/ex01/main.cpp
+++ CPP_6/ex01/main.cpp
@@ -12,15 +12,28 @@
 /*                                                 <__________\______)\__)      */
 /* **************************************************************************** */
 
-#include "Includes/SerializationHeader.hpp"
+#include "Include/SerializationHeader.hpp"
+
+static const std::string arrStr[] = {"First", "Second", "Third", "Four", "Five"};
 
 void* serialize(void){
 	std::srand(std::time(NULL));
-	int randIdx = rand() % 2;
+	int randIdx = rand() % 5;
 	Data *newData = new Data();
+	newData->str1 = arrStr[randIdx];
+	newData->str2 = arrStr[randIdx];
+	newData->integer = randIdx;
+	return newData;
+}
+
+Data* deserialize(void* raw){
+	return static_cast <Data *> (raw);
 }
 
 int main(){
-
+	void* dataSerialize1 = serialize();
+	Data* data1 = deserialize(dataSerialize1);
+	std::cout << data1->str1 << "|" << data1->str2 << "|" << data1->integer << std::endl;
+	delete data1;
 	return (0);
 }
