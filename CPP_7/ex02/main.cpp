00e06f592bf671a6f038d6900d950aead1cfb148
pichkasik <sasha.marakulin@mail.ru>
2021-05-22 22:32:34 +0300

    Add tests

--- CPP_7/ex02/main.cpp
+++ CPP_7/ex02/main.cpp
@@ -18,13 +18,22 @@ int main(){
 
 	Array<int> *intArray = new Array<int>(10);
 	Array<double> *doubleArray = new Array<double>(10);
-	for (int i = 0; i < intArray->size(); ++i)
-	{
+
+	for (int i = 0; i < intArray->size(); ++i){
 		(*intArray)[i] = i;
 		(*doubleArray)[i] = i + 0.5;
 	}
 
-	for (int i = 0; i < intArray->size(); ++i)
+	for (int i = 0; i < intArray->size(); ++i){
 		std::cout << (*intArray)[i] << "\t" << (*doubleArray)[i] << '\n';
+	}
+
+	int idx = 90;
+	try{
+		std::cout << (*intArray)[idx] << std::endl;
+	}
+	catch (std::exception &e){
+		std::cout << "[-] Error: " << idx << " " << e.what() << std::endl;
+	}
 	return (0);
 }
