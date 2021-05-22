7058568cca92f907546c396dae62954822a171d1
pichkasik <sasha.marakulin@mail.ru>
2021-05-22 19:18:50 +0300

    Add tests

--- CPP_7/ex01/main.cpp
+++ CPP_7/ex01/main.cpp
@@ -14,23 +14,35 @@
 
 #include "Include/iter.hpp"
 
-int multiply(int i){
-	return i * 2;
+
+void checkFloat(float i){
+	std::cout << "Float func - " << i << std::endl;
 }
 
-int toUpper(int c){
+void checkInt(int i){
+	std::cout << "Int func - " << i << std::endl;
+}
+
+int checkChar(int c){
+	std::cout << "Char func - " << c << std::endl;
 	return std::toupper(c);
 }
 
 bool reverseBool(bool b){
+	std::cout << "Bool func - " << b << std::endl;
 	return !b;
+}
+
+void checkStr(const std::string &s){
+	std::cout << s  << std::endl;
 }
 
 int main(){
 
-//	std::string arrString[] = {"fadsfasd" , "r123123r123r", "%^F&^I&I^G@", "R"};
+	std::string arrString[] = {"fadsfasd" , "r123123r123r", "%^F&^I&I^G@", "R"};
 	bool boolArr[4] = {true, true, false, false};
 	int* intArr = new int[4];
+	float floatArr[] = {.124, 235.15, 321523, 8.0};
 	char charArr[12] = "Hello world";
 
 	for (int i = 0; i < 4; i++){
@@ -38,35 +50,29 @@ int main(){
 	}
 	std::cout <<  std::endl << "=========INT ARR=========" << std::endl;
 	printArr(intArr, 4);
-	::iter(intArr, 4, multiply);
-	printArr(intArr, 4);
-	printArr(intArr, 4);
-	::iter(intArr, 4, toUpper);
-	printArr(intArr, 4);
+	iter(intArr, 4, checkInt);
+	iter(intArr, 4, checkChar);
+
+	std::cout <<  std::endl << "=========FLOAT ARR=========" << std::endl;
+	printArr(floatArr, 4);
+	iter(floatArr, 4, checkFloat);
+	iter(floatArr, 4, checkChar);
 
 	std::cout <<  std::endl << "=========CHAR ARR=========" << std::endl;
 	printArr(charArr, 12);
-	::iter(charArr, 12, toUpper);
-	printArr(charArr, 12);
-	printArr(charArr, 12);
-	::iter(charArr, 12, reverseBool);
-	printArr(charArr, 12);
+	iter(charArr, 12, checkChar);
+	iter(charArr, 12, reverseBool);
 
 	std::cout <<  std::endl << "=========BOOL ARR=========" << std::endl;
 	printArr(boolArr, 4);
-	::iter(boolArr, 4, reverseBool);
-	printArr(boolArr, 4);
-	printArr(boolArr, 4);
-	::iter(boolArr, 4, multiply);
-	printArr(boolArr, 4);
+	iter(boolArr, 4, reverseBool);
+	iter(boolArr, 4, checkInt);
 
-//	std::cout <<  std::endl << "=========Str ARR=========" << std::endl;
-//	printArr(arrString, 4);
-//	::iter(arrString, 4, toUpper);
-//	printArr(arrString, 4);
-//	printArr(arrString, 4);
-//	::iter(arrString, 4, multiply);
-//	printArr(arrString, 4);
+	std::cout <<  std::endl << "=========Str ARR=========" << std::endl;
+	printArr(arrString, 4);
+	iter(arrString, 4, checkStr);
+	iter(arrString, 4, checkStr);
+
 
 	return (0);
 }
