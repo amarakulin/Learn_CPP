b31a62bcd42a39b68cabe828f43a59554ba31ab8
pichkasik <sasha.marakulin@mail.ru>
2021-05-23 18:35:27 +0300

    add tests

--- CPP_8/ex00/main.cpp
+++ CPP_8/ex00/main.cpp
@@ -14,7 +14,42 @@
 
 #include "easyfind.h"
 
+template <typename T, typename V>
+void testEasyFind(T container, V value){
+	try {
+		std::cout << *easyfind(container, value) << std::endl;
+	}
+	catch (std::exception&e){
+		std::cout << value << " " << e.what() << std::endl;
+	}
+}
+
 int main(){
+
+	std::vector<int> intVector ;
+	std::vector<char> charVector ;
+	std::vector<float> floatVector ;
+	std::vector<std::string> strVector ;
+
+
+
+	std::cout << "=================TEST INT VECTOR=================" << std::endl;
+	intVector.push_back(10);
+	intVector.push_back(20);
+	intVector.push_back(30);
+	testEasyFind(intVector, 11);
+	testEasyFind(intVector, 10);
+	testEasyFind(intVector, 20);
+	testEasyFind(intVector, 40);
+
+	std::cout << "=================TEST CHAR VECTOR=================" << std::endl;
+	charVector.push_back('f');
+	charVector.push_back('s');
+	charVector.push_back('q');
+	testEasyFind(charVector, 'f');
+	testEasyFind(charVector, 'f');
+	testEasyFind(charVector, 's');
+	testEasyFind(charVector, 'r');
 
 	return (0);
 }
