c51d927c1c156ef08d51fc75898d9aea4f2d2bcb
pichkasik <sasha.marakulin@mail.ru>
2021-05-23 13:48:35 +0300

    Add test

--- CPP_7/ex00/main.cpp
+++ CPP_7/ex00/main.cpp
@@ -12,7 +12,32 @@
 /*                                                 <__________\______)\__)      */
 /* **************************************************************************** */
 
+#include <ostream>
 #include "Include/whatever.hpp"
+
+class A{
+public:
+	A(int n): _n(n){}
+	bool operator==(A const & rhs) const {return (this->_n == rhs._n);}
+	bool operator!=(A const & rhs) const {return (this->_n != rhs._n);}
+	bool operator>(A const & rhs) const {return (this->_n > rhs._n);}
+	bool operator<(A const & rhs) const {return (this->_n < rhs._n);}
+	bool operator>=(A const & rhs) const {return (this->_n >= rhs._n);}
+	bool operator<=(A const & rhs) const {return (this->_n <= rhs._n);}
+
+	int getN() const{
+		return _n;
+	}
+
+private:
+	int _n;
+};
+
+std::ostream &operator<<(std::ostream &os, const A &a)
+{
+	os << "_n: " << a.getN();
+	return os;
+}
 
 int main(){
 	int a = 3;
@@ -23,6 +48,10 @@ int main(){
 	float f2 = 5.1119;
 	char ch1 = 'a';
 	char ch2 = 'b';
+	A a1(1);
+	A a2(2);
+
+
 	::swap( a, b );
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
@@ -41,11 +70,15 @@ int main(){
 	std::cout << "min( t, f ) = " << ::min( t, f ) << std::endl;
 	std::cout << "min( f1, f2 ) = " << ::min( f1, f2 ) << std::endl;
 	std::cout << "min( ch1, ch2 ) = " << ::min( ch1, ch2 ) << std::endl;
+	std::cout << "min( a1, a2 ) = " << ::min( a1, a2 ) << std::endl;
+
 
 	std::cout <<  std::endl << "=========MAX=========" << std::endl;
 	std::cout << "max( t, f ) = " << ::max( t, f ) << std::endl;
 	std::cout << "max( f1, f2 ) = " << ::max( f1, f2 ) << std::endl;
 	std::cout << "max( ch1, ch2 ) = " << ::max( ch1, ch2 ) << std::endl;
+	std::cout << "max( a1, a2 ) = " << ::max( a1, a2 ) << std::endl;
+
 
 	std::cout <<  std::endl << "=========SWAP=========" << std::endl;
 	std::cout << "Before: t = " << t << ", f = " << f << std::endl;
@@ -57,5 +90,9 @@ int main(){
 	std::cout << "Before: ch1 = " << ch1 << ", ch2 = " << ch2 << std::endl;
 	::swap(ch1, ch2);
 	std::cout << "After: ch1 = " << ch1 << ", ch2 = " << ch2 << std::endl;
+
+	std::cout << "Before: a1 = " << a1 << ", a2 = " << a2 << std::endl;
+	::swap(a1, a2);
+	std::cout << "After: a1 = " << a1 << ", a2 = " << a2 << std::endl;
 	return (0);
 }
