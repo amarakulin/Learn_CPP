56b0dd823856a8bf98da247592d7291429207a99
pichkasik <sasha.marakulin@mail.ru>
2021-05-23 22:07:01 +0300

    Add tests

--- CPP_8/ex01/main.cpp
+++ CPP_8/ex01/main.cpp
@@ -16,12 +16,22 @@
 
 int main()
 {
+	Span spSubj = Span(5);
+	spSubj.addNumber(5);
+	spSubj.addNumber(3);
+	spSubj.addNumber(17);
+	spSubj.addNumber(9);
+	spSubj.addNumber(11);
+	std::cout << spSubj.shortestSpan() << std::endl;
+	std::cout << spSubj.longestSpan() << std::endl;
+	
+	std::cout << "============MY TESTS============" << std::endl;
 	Span sp = Span(5);
-	sp.addNumber(5);
+	sp.addNumber(-5);
 	sp.addNumber(3);
-	sp.addNumber(17);
+	sp.addNumber(-17);
 	sp.addNumber(9);
-	sp.addNumber(11);
+	sp.addNumber(-11);
 	std::cout << sp.shortestSpan() << std::endl;
 	std::cout << sp.longestSpan() << std::endl;
 }
