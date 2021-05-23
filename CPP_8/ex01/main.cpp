6fa261a585017ae64cc50a3848e7695699324b2e
pichkasik <sasha.marakulin@mail.ru>
2021-05-23 21:12:00 +0300

    Add subj tests

--- CPP_8/ex01/main.cpp
+++ CPP_8/ex01/main.cpp
@@ -12,4 +12,16 @@
 /*                                                 <__________\______)\__)      */
 /* **************************************************************************** */
 
+#include "Span.hpp"
 
+int main()
+{
+	Span sp = Span(5);
+	sp.addNumber(5);
+	sp.addNumber(3);
+	sp.addNumber(17);
+	sp.addNumber(9);
+	sp.addNumber(11);
+	std::cout << sp.shortestSpan() << std::endl;
+	std::cout << sp.longestSpan() << std::endl;
+}
