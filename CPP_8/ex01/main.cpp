d17fd93d920b6389500e6e0b85f0b865b4693aaa
pichkasik <sasha.marakulin@mail.ru>
2021-05-23 22:38:48 +0300

    add tests

--- CPP_8/ex01/main.cpp
+++ CPP_8/ex01/main.cpp
@@ -16,6 +16,7 @@
 
 int main()
 {
+	std::srand(std::time(NULL));
 	Span spSubj = Span(5);
 	spSubj.addNumber(5);
 	spSubj.addNumber(3);
@@ -26,12 +27,34 @@ int main()
 	std::cout << spSubj.longestSpan() << std::endl;
 	
 	std::cout << "============MY TESTS============" << std::endl;
+	std::cout << "============NEGATIVE NUMBERS============" << std::endl;
 	Span sp = Span(5);
-	sp.addNumber(-5);
-	sp.addNumber(3);
-	sp.addNumber(-17);
-	sp.addNumber(9);
-	sp.addNumber(-11);
+	try{
+		sp.addNumber(-5);
+		sp.addNumber(3);
+		sp.addNumber(-17);
+		sp.addNumber(9);
+		sp.addNumber(-11);
+
+		sp.addNumber(-11);
+	}
+	catch (std::exception &e){
+		std::cout <<  e.what() << std::endl;
+	}
 	std::cout << sp.shortestSpan() << std::endl;
 	std::cout << sp.longestSpan() << std::endl;
+
+	std::cout << "============CREATE BIG COLLECTION============" << std::endl;
+	int size = 10;
+	Span spTen = Span(size);
+	try{
+		spTen.addRundomNumbers(size);
+	}
+	catch (std::exception &e){
+		std::cout << e.what() << std::endl;
+	}
+
+	std::cout << spTen.shortestSpan() << std::endl;
+	std::cout << spTen.longestSpan() << std::endl;
+	spTen.printCollection();
 }
