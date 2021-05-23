86859e1dc213edd8a7ad176792f9ba50ff720763
pichkasik <sasha.marakulin@mail.ru>
2021-05-23 13:35:36 +0300

    Change destructor

--- CPP_7/ex02/Include/Array.hpp
+++ CPP_7/ex02/Include/Array.hpp
@@ -68,10 +68,7 @@ public:
 	}
 
 	void deleteArr(){
-		for (int i = 0; i < _size; i++){
-			delete _arrElements[i];
-		}
-		delete _arrElements;
+		delete[] _arrElements;
 		_arrElements = nullptr;
 	}
 
@@ -79,85 +76,5 @@ private:
 	T* _arrElements;
 	int _size;
 };
-
-
-
-//template<class T>
-//Array<T>::Array(){
-//	_arrElements = new T();
-//	_size = 0;
-//}
-
-//template<class T>
-//Array<T>::Array(unsigned int n){
-//	_arrElements = new T[n];
-//	std::fill(_arrElements, _arrElements + n, 0);
-//	_size = static_cast <int> (n);
-//}
-
-//template<class T>
-//Array<T>::Array(const Array &copy){
-//	operator=(copy);
-//}
-
-//template<class T>
-//Array<T> &Array<T>::operator=(const Array &assign){
-//	if (this != &assign){
-//		deleteArr();
-//		_arrElements = new T[assign._size];
-//		std::copy(assign, assign, _arrElements);
-//	}
-//	return *this;
-//}
-
-//template<class T>
-//T &Array<T>::operator[](int &idx){
-//	try{
-//		if (idx >= _size || idx < 0){
-//			throw NotExistingElementException();
-//		}
-//		return _arrElements[idx];
-//	}
-//	catch (std::exception &e){
-//		std::cout << "[-] Error: " << idx << " " << e.what() << std::endl;
-//	}
-//}
-
-//template<class T>
-//Array<T>::~Array(){
-//	deleteArr();
-//}
-
-//template<class T>
-//void Array<T>::deleteArr(){
-//	for (int i = 0; i < _size; i++){
-//		delete _arrElements[i];
-//	}
-//	delete _arrElements;
-//	_arrElements = nullptr;
-//}
-
-//template<class T>
-//int Array<T>::size(){
-//	return _size;
-//}
-
-//template<class T>
-//T *Array<T>::getArrElements() const{
-//	return _arrElements;
-//}
-
-//template<class T>
-//const char *Array<T>::NotExistingElementException::what() const throw(){
-//	return "Not Existing Element";
-//}
-
-
-//template<class T>
-//std::ostream &operator<<(std::ostream &os, const Array<T> &array)
-//{
-//	os << "_arrElements: " << array._arrElements << " _size: " << array._size;
-//	return os;
-//}
 
 #endif
