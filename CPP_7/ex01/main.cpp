8c78d1f7171e23b0326ae3a21c5d2957ace48279
pichkasik <sasha.marakulin@mail.ru>
2021-05-23 14:10:20 +0300

    Add tests

--- CPP_7/ex01/main.cpp
+++ CPP_7/ex01/main.cpp
@@ -14,23 +14,44 @@
 
 #include "Include/iter.hpp"
 
+class A{
+public:
+	A(void): _n(42){}
+//	bool operator==(A const & rhs) const {return (this->_n == rhs._n);}
+//	bool operator!=(A const & rhs) const {return (this->_n != rhs._n);}
+//	bool operator>(A const & rhs) const {return (this->_n > rhs._n);}
+//	bool operator<(A const & rhs) const {return (this->_n < rhs._n);}
+//	bool operator>=(A const & rhs) const {return (this->_n >= rhs._n);}
+//	bool operator<=(A const & rhs) const {return (this->_n <= rhs._n);}
 
-void checkFloat(float i){
+	int getN() const{
+		return _n;
+	}
+
+private:
+	int _n;
+};
+
+std::ostream &operator<<(std::ostream &os, const A &a)
+{
+	os << "_n: " << a.getN();
+	return os;
+}
+
+void checkFloat(const float &i){
 	std::cout << "Float func - " << i << std::endl;
 }
 
-void checkInt(int i){
+void checkInt(const int &i){
 	std::cout << "Int func - " << i << std::endl;
 }
 
-int checkChar(int c){
+void checkChar(const char &c){
 	std::cout << "Char func - " << c << std::endl;
-	return std::toupper(c);
 }
 
-bool reverseBool(bool b){
+void checkBool(const bool &b){
 	std::cout << "Bool func - " << b << std::endl;
-	return !b;
 }
 
 void checkStr(const std::string &s){
@@ -51,28 +72,30 @@ int main(){
 	std::cout <<  std::endl << "=========INT ARR=========" << std::endl;
 	printArr(intArr, 4);
 	iter(intArr, 4, checkInt);
-	iter(intArr, 4, checkChar);
 
 	std::cout <<  std::endl << "=========FLOAT ARR=========" << std::endl;
 	printArr(floatArr, 4);
 	iter(floatArr, 4, checkFloat);
-	iter(floatArr, 4, checkChar);
 
 	std::cout <<  std::endl << "=========CHAR ARR=========" << std::endl;
 	printArr(charArr, 12);
 	iter(charArr, 12, checkChar);
-	iter(charArr, 12, reverseBool);
 
 	std::cout <<  std::endl << "=========BOOL ARR=========" << std::endl;
 	printArr(boolArr, 4);
-	iter(boolArr, 4, reverseBool);
-	iter(boolArr, 4, checkInt);
+	iter(boolArr, 4, checkBool);
 
 	std::cout <<  std::endl << "=========Str ARR=========" << std::endl;
 	printArr(arrString, 4);
 	iter(arrString, 4, checkStr);
-	iter(arrString, 4, checkStr);
 
+	std::cout <<  std::endl << "=========A ARR=========" << std::endl;
+	int tab[] = {0, 1, 2, 3, 4};
+	A tab2[5];
+	iter(tab, 5, print);
+	iter(tab2, 5, print);
+
+	delete[] intArr;
 
 	return (0);
 }
