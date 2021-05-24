a6c91d23dd1b9b6425e91dc5af1b00009f28d550
pichkasik <sasha.marakulin@mail.ru>
2021-05-24 17:02:48 +0300

    Inherit iterator

--- CPP_8/ex02/MutantStack.hpp
+++ CPP_8/ex02/MutantStack.hpp
@@ -17,37 +17,60 @@
 #define CPP_8_MUTANTSTACK_HPP
 
 #include <iostream>
+#include <algorithm>
 #include <stack>
+#include <queue>
 
-template <class T>
-class MutantStack : public std::stack<typename T::value_type, T>{
+//template <typename T>
+//class iterator : public std::iterator<std::bidirectional_iterator_tag, T>{
+//public:
+//	void test(){}
+//
+//	iterator begin(){
+//		return ;
+//	}
+//	iterator end(){
+//		return ;
+//	}
+//};
+
+template <typename T, class Container = std::deque<T> >
+class MutantStack : public std::stack<T, Container>{
 public:
-	MutantStack();
-	MutantStack(MutantStack const &copy);
-	MutantStack& operator=(const MutantStack &assign);
-	~MutantStack();
+	typedef typename std::stack<T, Container>::container_type::iterator iterator;
+	iterator &begin(){
+		return (iterator(this->c[0]));
+	}
 
+	iterator &end(){
+		return (iterator(this->c[static_cast <int> (this->size())]));
+	}
+
+
+
+
+
+	MutantStack()
+	{
+
+	}
+
+	MutantStack(MutantStack const &copy)
+	{
+
+	}
+
+	MutantStack& operator=(const MutantStack &assign)
+	{
+		return ;
+	}
+
+	~MutantStack()
+	{
+
+	}
 };
 
-template<class T>
-MutantStack<T>::MutantStack(){
-
-}
-
-template<class T>
-MutantStack<T>::MutantStack(const MutantStack &copy){
-
-}
-
-template<class T>
-MutantStack<T> &MutantStack<T>::operator=(const MutantStack &assign){
-	return ;
-}
-
-template<class T>
-MutantStack<T>::~MutantStack(){
-
-}
 
 
 #endif
