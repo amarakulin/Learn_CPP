88ef021fc0a9e06f1aedfe9c7a3bdfff5e9746ac
pichkasik <sasha.marakulin@mail.ru>
2021-05-24 22:31:34 +0300

    add tests

--- CPP_8/ex02/main.cpp
+++ CPP_8/ex02/main.cpp
@@ -12,11 +12,100 @@
 /*                                                 <__________\______)\__)      */
 /* **************************************************************************** */
 
-
 #include "MutantStack.hpp"
+
+//void testIntList(){
+//	MutantStack<int> mstack;
+//	mstack.push("Hello");
+//	mstack.push("as");
+//	mstack.push("tcyvbu");
+//	mstack.push("5.2");
+//	mstack.push("La");
+//	mstack.push("|");
+//	MutantStack<int>::iterator it = mstack.begin();
+//	MutantStack<int>::iterator ite = mstack.end();
+//	while (it != ite)
+//	{
+//		std::cout << it << std::endl;
+//		++it;
+//	}
+//	std::cout << "Top -> " << mstack.top() << std::endl;
+//	std::cout << "Size -> " << mstack.size() << std::endl;
+//	std::list<std::string> s(mstack);
+//	std::cout << "Size of copy-> " << s.size() << std::endl;
+////	std::cout << "Top of copy-> " << s.top() << std::endl;
+//}
+//
+//void testStringList(){
+//	MutantStack<std::string> mstack;
+//	mstack.push("Hello");
+//	mstack.push("as");
+//	mstack.push("tcyvbu");
+//	mstack.push("5.2");
+//	mstack.push("La");
+//	mstack.push("|");
+//	MutantStack<std::string>::iterator it = mstack.begin();
+//	MutantStack<std::string>::iterator ite = mstack.end();
+//	while (it != ite)
+//	{
+//		std::cout << it << std::endl;
+//		++it;
+//	}
+//	std::cout << "Top -> " << mstack.top() << std::endl;
+//	std::cout << "Size -> " << mstack.size() << std::endl;
+//	std::list<std::string> s(mstack);
+//	std::cout << "Size of copy-> " << s.size() << std::endl;
+////	std::cout << "Top of copy-> " << s.top() << std::endl;
+//}
+
+void testString(){
+	MutantStack<std::string> mstack;
+	mstack.push("Hello");
+	mstack.push("as");
+	mstack.push("tcyvbu");
+	mstack.push("5.2");
+	mstack.push("La");
+	mstack.push("|");
+	MutantStack<std::string>::iterator it = mstack.begin();
+	MutantStack<std::string>::iterator ite = mstack.end();
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::cout << "Top -> " << mstack.top() << std::endl;
+	std::cout << "Size -> " << mstack.size() << std::endl;
+	std::stack<std::string> s(mstack);
+	std::cout << "Size of copy-> " << s.size() << std::endl;
+	std::cout << "Top of copy-> " << s.top() << std::endl;
+
+}
+
+void testFloat(){
+	MutantStack<float> mstack;
+	mstack.push(5.12);
+	mstack.push(17);
+	mstack.push(3);
+	mstack.push(5.2);
+	mstack.push(737);
+	mstack.push(.567);
+	MutantStack<float>::iterator it = mstack.begin();
+	MutantStack<float>::iterator ite = mstack.end();
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::cout << "Top -> " << mstack.top() << std::endl;
+	std::cout << "Size -> " << mstack.size() << std::endl;
+	std::stack<float> s(mstack);
+	std::cout << "Size of copy-> " << s.size() << std::endl;
+	std::cout << "Top of copy-> " << s.top() << std::endl;
+}
 
 int main()
 {
+	std::cout << "=================Subj test=================" << std::endl;
 	MutantStack<int> mstack;
 	mstack.push(5);
 	mstack.push(17);
@@ -26,7 +115,6 @@ int main()
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-//[...]
 	mstack.push(0);
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
@@ -38,5 +126,13 @@ int main()
 		++it;
 	}
 	std::stack<int> s(mstack);
+	std::cout << s.size() << std::endl;
+	std::cout << "=================My tests=================" << std::endl;
+	std::cout << "=================test float stack=================" << std::endl;
+	testFloat();
+	std::cout << "=================test string stack=================" << std::endl;
+	testString();
+//	std::cout << "=================test string list=================" << std::endl;
+//	testStringList();
 	return 0;
 }
